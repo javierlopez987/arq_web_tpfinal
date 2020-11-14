282e64c1e0e8ca52f3aa83655ab25b3473accda2
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-14 11:33:26 -0300

    microservicio usuario

---
+++ usuario-1/src/test/java/edu/tudai/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tudai.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
